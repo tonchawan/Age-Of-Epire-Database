bc2e5e1a315e5e5efb3078db881fe99131de9dfb
tonchawan <tonchawan50@gmail.com>
2022-12-02 16:39:39 +0700

    Slove React router ploblem

--- aoe-app/src/App.js
+++ aoe-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home-Page/Component/Home/Home";
+import "./App.css";
+import {Route, Routes, Link} from "react-router-dom"
+import { useState } from "react";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [price, setPrice] = useState()
+
+    return (
+      <div>
+        <nav>
+          <Link to= "/ ">
+          <img
+            src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"
+            alt=""
+          />
+          <h1>Bitcoin prices</h1>
+          </Link>
+
+        </nav>
+
+        <main>
+          <Routes>
+            <Route path = "/" element ={<Home />} />
+          </Routes>
+        </main>
+
+      </div>
+    );
 }
 
 export default App;
---
+++ aoe-app/src/Home-Page/Component/Home/Home.js
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+      <div>
+        <h1>This is the homepage!</h1>
+        <p>Where all of your bitcoin dreams can come true</p>
+      </div>
+    );
+}
+
+export default Home;
